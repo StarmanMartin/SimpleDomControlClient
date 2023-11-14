f38339b2b3a44e84bfbc05e759ec187185886667
Martin Starman <m.starman@live.com>
2023-11-14 10:31:19 +0100

    v0.3.3

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import {AbstractSDC} from './simpleDomControl/AbstractSDC.js';
 import {on, trigger, allOff, setEvent} from './simpleDomControl/sdc_events.js';
 import {clearErrorsInForm, setErrorsInForm, checkIfParamNumberBoolOrString} from './simpleDomControl/sdc_utils.js';
 import {controllerFactory, runControlFlowFunctions} from './simpleDomControl/sdc_controller.js';
+import {close} from './simpleDomControl/sdc_socket.js'
 
+const socketReconnect = close
 
 export {
     app, AbstractSDC, on, trigger, allOff, setEvent, clearErrorsInForm, setErrorsInForm, checkIfParamNumberBoolOrString,
-    controllerFactory, runControlFlowFunctions
+    controllerFactory, runControlFlowFunctions, socketReconnect
 }
