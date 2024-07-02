92a91d7b53f1a84b812ac44c7c42bc140d7b2c17
martin <m.starman@live.com>
2024-07-02 13:20:56 +0200

    - Added docu - update mode -> added SDCMeta class - update gulp -> added copy static

--- gulp/gulp.jsx
+++ gulp/gulp.jsx
@@ -21,7 +21,8 @@ function scss() {
  * @returns {*}
  */
 function pre_compile_javascript() {
-    return src(['.js', '.json'].map((x)=> `./src/**/*${x}`), {follow: true})
+    let file_extentions = process.env.JS_CILENT_FILE_EXTENTIONS || ['.js', '.json'];
+    return src(file_extentions.map((x)=> `./src/**/*${x}`), {follow: true})
         .pipe(through.obj(function (obj, enc, next) {
             let srcFile = obj.path
             if (!obj.isNull() && !obj.isDirectory() && obj.isBuffer() && /.js$/.test(srcFile)) {
